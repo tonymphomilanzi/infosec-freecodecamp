37a80abab51505001036222914c30972763480ce
Tony Milanzi <113247305+tonymphomilanzi@users.noreply.github.com>
2025-07-03 22:23:06 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express     = require('express');
 const bodyParser  = require('body-parser');
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
 const app         = express();
-const bcrypt      = require('bcrypt');
+const bcrypt = require('bcrypt');
 fccTesting(app);
 const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
@@ -14,7 +14,8 @@ app.get('/', (req, res) => {
 })
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
-
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+  console.log(hash);
 
 //END_ASYNC
 
